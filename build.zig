861f309cd81cfdbbe80a5e239679d86f815ac157
Ashley Towns <mail@ashleytowns.id.au>
2024-12-16 02:44:15 +1100

    wip

--- build.zig
+++ build.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const Build = std.Build;
+const OptimizeMode = std.builtin.OptimizeMode;
+const sokol = @import("sokol");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -20,20 +24,43 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "test",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
+    const dep_zalgebra = b.dependency("zalgebra", .{
+        .target = target,
+        .optimize = optimize
+    });
+
+    const deps = [_]struct{[]const u8, *Build.Dependency}{
+        .{"zalgebra", dep_zalgebra}
+    };
+
+    buildShaders(b, target);
+
+    if (target.result.isWasm()) {
+        try buildWeb(b, target, optimize, dep_sokol);
+    } else {
+        try buildNative(b, target, optimize, dep_sokol, &deps);
+    }
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+fn buildNative(b: *Build,
+               target: Build.ResolvedTarget,
+               optimize: OptimizeMode,
+               dep_sokol: *Build.Dependency,
+               other_deps: []const struct{ []const u8, *Build.Dependency }) !void {
     const exe = b.addExecutable(.{
         .name = "test",
         .root_source_file = b.path("src/main.zig"),
@@ -42,6 +69,10 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.root_module.addImport("sokol", dep_sokol.module("sokol"));
+
+    for (other_deps) |dep| {
+      exe.root_module.addImport(dep[0],dep[1].module(dep[0]));
+    }
 
     exe.linkLibC();
 
@@ -72,30 +103,69 @@ pub fn build(b: *std.Build) void {
     // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+}
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
+fn buildWeb(b: *Build, target: Build.ResolvedTarget, optimize: OptimizeMode, dep_sokol: *Build.Dependency) !void {
+    const exe = b.addExecutable(.{
+        .name = "test",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    exe.root_module.addImport("sokol", dep_sokol.module("sokol"));
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    const emsdk = dep_sokol.builder.dependency("emsdk", .{});
+    const link_step = try sokol.emLinkStep(b, .{
+        .lib_main = exe,
+        .target = target,
+        .optimize = optimize,
+        .emsdk = emsdk,
+        .use_webgl2 = true,
+        .use_emmalloc = true,
+        .use_filesystem = false,
+        .shell_file_path = dep_sokol.path("src/sokol/web/shell.html"),
+    });
+
+    const run = sokol.emRunStep(b, .{ .name = "test", .emsdk = emsdk });
+    run.step.dependOn(&link_step.step);
+    b.step("run", "Run test").dependOn(&run.step);
 }
 
+fn buildShaders(b: *Build, target: Build.ResolvedTarget) void {
+    std.debug.print("Building Shaders", .{});
+    const sokol_tools_bin_dir = "../sokol-tools-bin/bin/";
+    const shaders_dir = "src/shaders/";
+    const shaders = .{
+        "test.glsl",
+    };
+    const optional_shdc: ?[:0]const u8 = comptime switch (builtin.os.tag) {
+        .windows => "win32/sokol-shdc.exe",
+        .linux => "linux/sokol-shdc",
+        .macos => if (builtin.cpu.arch.isX86()) "osx/sokol-shdc" else "osx_arm64/sokol-shdc",
+        else => null,
+    };
+    if (optional_shdc == null) {
+        std.log.warn("unsupported host platform, skipping shader compiler step", .{});
+        return;
+    }
+    const shdc_path = sokol_tools_bin_dir ++ optional_shdc.?;
+    const shdc_step = b.step("shaders", "Compile shaders (needs ../sokol-tools-bin)");
+    const glsl = if (target.result.isDarwin()) "glsl410" else "glsl430";
+    const slang = glsl ++ ":metal_macos:hlsl5:glsl300es:wgsl";
+    inline for (shaders) |shader| {
+        const cmd = b.addSystemCommand(&.{
+            shdc_path,
+            "-i",
+            shaders_dir ++ shader,
+            "-o",
+            shaders_dir ++ shader ++ ".zig",
+            "-l",
+            slang,
+            "-f",
+            "sokol_zig",
+            "--reflection",
+        });
+        shdc_step.dependOn(&cmd.step);
+    }
+}
--- src/main.zig
+++ src/main.zig
@@ -1,103 +1,218 @@
-const rl = @import("raylib");
-
+const sokol = @import("sokol");
+const slog = sokol.log;
+const sg = sokol.gfx;
+const sapp = sokol.app;
+const sdtx = sokol.debugtext;
+const sglue = sokol.glue;
+const stm = sokol.time;
+const sshape = sokol.shape;
+const za = @import("zalgebra");
 const std = @import("std");
 
-fn positionVector(shape: rl.Rectangle) rl.Vector2 {
-    return struct {
-        .x = shape.x,
-        .y = shape.y
-    };
-}
+const Vec3 = za.Vec3;
+const Vec2 = za.Vec2;
 
-const Entity = struct {
-    var shape: rl.Rectangle = rl.Rectangle { 0 };
+const Rectangle = struct {
+    position: Vec2,
+    size: Vec2,
 
-    inline fn camera() rl.Camera2D {
-        return rl.Camera2D {
-            .target = shape,
-            .offset = rl.Vector2 { .x = 200, .y = 200 },
-            .rotation = 0,
-            .zoom = 1.0
+    fn new(x: f32, y: f32, w: f32, h: f32) Rectangle {
+        return Rectangle {
+            .position = Vec2.new(x, y),
+            .size = Vec2.new(w, h)
         };
+    }
+
+    fn zero() Rectangle {
+        return new(0, 0, 0, 0);
     }
 };
 
-pub fn MakeEntity(position: rl.Vector2, size: rl.Vector2) Entity {
+const Entity = struct {
+    var shape: Rectangle = Rectangle.zero();
+
+    // inline fn camera() rl.Camera2D {
+    //     return rl.Camera2D {
+    //         .target = shape,
+    //         .offset = rl.Vector2 { .x = 200, .y = 200 },
+    //         .rotation = 0,
+    //         .zoom = 1.0
+    //     };
+    // }
+};
+
+pub fn MakeEntity(position: @Vector(2, f32), size: Vec2) Entity {
     return Entity { position, size };
 }
 
+const kScreenWidth = 800;
+const kScreenHeight = 600;
+
+const state = struct {
+    var pass_action = sg.PassAction {};
+    var time_stamp: u64 = 0;
+    var prim = struct {
+        ibuf: sg.Buffer,
+        pip: sg.Pipeline
+    };
+};
+
+export fn init() void {
+    stm.setup();
+    sg.setup(.{
+        .environment = sglue.environment(),
+        .logger = .{ .func = slog.func }
+    });
+
+    var sdtx_desc = sdtx.Desc { .logger = .{ .func = slog.func } };
+    sdtx_desc.fonts[0] = sdtx.fontKc853();
+    sdtx.setup(sdtx_desc);
+
+    const pip_desc = sg.PipelineDesc {
+        .layout = {
+            .attrs[0].format = sg.VertexFormat.FLOAT2;
+            .attrs[1].format = sg.VertexFormat.UBYTE4N;
+        },
+        .depth = .{
+            .write_enabled = true,
+            .compare = sg.CompareFunc.LESS_EQUAL
+        },
+        .index_type = sg.IndexType.UINT16,
+        .primitive_type = sg.PrimitiveType.LINES,
+    };
+
+    state.prim.ibuf = sg.makeBuffer();
+    _ = sg.makePipeline(pip_desc);
+    
+
+    state.pass_action.colors[0] = sg.ColorAttachmentAction { .load_action = .CLEAR, .clear_value = sg.Color { .r = 0, .g = 0.125, .b = 0.25, .a = 1 } };
+}
+
+export fn frame() void {
+    const frame_time = stm.ms(stm.laptime(&state.time_stamp));
+
+    sdtx.canvas(@as(f32, @floatFromInt(sapp.width())) * 0.5, @as(f32, @floatFromInt(sapp.height())) * 0.5);
+    sdtx.origin(0.0, 2.0);
+
+    sdtx.font(0);
+    sdtx.color3b(100, 80, 120);
+    sdtx.puts("Hello\n");
+    sdtx.print("Frame Time: {d:.3}ms\n", .{frame_time});
+
+    sg.beginPass(sg.Pass { .action = state.pass_action, .swapchain = sglue.swapchain() });
+    sdtx.draw();
+
+                 for (0..kScreenWidth/40) |i| {
+                     const startVector = Vec2.new(40.0*i, 0);
+                     const endVector = Vec2.new(40.0*i, kScreenHeight);
+
+    //                 const startVector = rl.Vector2 { .x = @as(f32, @floatFromInt(40*i)), .y = 0 };
+    //                 const endVector = rl.Vector2 { .x = @as(f32, @floatFromInt(40*i)), .y = kScreenHeight };
+
+    //                 rl.drawLineV(startVector, endVector, rl.Color.light_gray);
+                 }
+
+    //             for (0..kScreenHeight/40) |i| {
+    //                 const startVector = rl.Vector2 { .y = @as(f32, @floatFromInt(40*i)), .x = 0 };
+    //                 const endVector = rl.Vector2 { .y = @as(f32, @floatFromInt(40*i)), .x = kScreenWidth };
+
+    //                 rl.drawLineV(startVector, endVector, rl.Color.light_gray);
+    //             }
+
+
+    sg.endPass();
+    sg.commit();
+}
+export fn input(event: ?*const sapp.Event) void {
+    _ = event; // autofix
+}
+export fn cleanup() void {
+    sdtx.shutdown();
+    sg.shutdown();
+}
+
 pub fn main() !void {
-    const kScreenWidth = 800;
-    const kScreenHeight = 600;
+    sapp.run(.{
+        .init_cb = init,
+        .frame_cb = frame,
+        .event_cb = input,
+        .cleanup_cb = cleanup,
+        .width = kScreenWidth,
+        .height = kScreenHeight,
+        .sample_count = 4,
+        .icon = .{ .sokol_default = true },
+        .window_title = "test",
+        .logger = .{.func = slog.func }
+    });
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    rl.initWindow(kScreenWidth, kScreenHeight, "testing");
-    defer rl.closeWindow();
+    // rl.initWindow(kScreenWidth, kScreenHeight, "testing");
+    // defer rl.closeWindow();
 
-    rl.setTargetFPS(60);
+    // rl.setTargetFPS(60);
 
-    var player: rl.Rectangle = rl.Rectangle { .height = 40, .width = 40, .x = 200, .y = 200 };
+    // var player: rl.Rectangle = rl.Rectangle { .height = 40, .width = 40, .x = 200, .y = 200 };
 
 
-    const camera: rl.Camera2D = rl.Camera2D{
-        .target = rl.Vector2 { .x = player.x, .y = player.y },
-        .offset = rl.Vector2 { .x = 200, .y = 200 },
-        .rotation = 0,
-        .zoom = 1.0,
-    };
+    // const camera: rl.Camera2D = rl.Camera2D{
+    //     .target = rl.Vector2 { .x = player.x, .y = player.y },
+    //     .offset = rl.Vector2 { .x = 200, .y = 200 },
+    //     .rotation = 0,
+    //     .zoom = 1.0,
+    // };
 
-    const sourceRectangle = rl.Rectangle { .height = -kScreenHeight, .width = kScreenWidth, .x = 0, .y = 0};
+    // const sourceRectangle = rl.Rectangle { .height = -kScreenHeight, .width = kScreenWidth, .x = 0, .y = 0};
 
-    const screenCamera1 = rl.loadRenderTexture(kScreenWidth, kScreenHeight);
-    defer rl.unloadRenderTexture(screenCamera1);
+    // const screenCamera1 = rl.loadRenderTexture(kScreenWidth, kScreenHeight);
+    // defer rl.unloadRenderTexture(screenCamera1);
 
-    while (!rl.windowShouldClose()) {
-        if (rl.isKeyDown(rl.KeyboardKey.key_s)) {
-            player.y += 3;
-        }
-        {
-            rl.beginTextureMode(screenCamera1);
-            defer rl.endTextureMode();
-            {
-                camera.begin();
-                defer camera.end();
-                rl.clearBackground(rl.Color.ray_white);
+    // while (!rl.windowShouldClose()) {
+    //     if (rl.isKeyDown(rl.KeyboardKey.key_s)) {
+    //         player.y += 3;
+    //     }
+    //     {
+    //         rl.beginTextureMode(screenCamera1);
+    //         defer rl.endTextureMode();
+    //         {
+    //             camera.begin();
+    //             defer camera.end();
+    //             rl.clearBackground(rl.Color.ray_white);
 
-                for (0..kScreenWidth/40) |i| {
-                    const startVector = rl.Vector2 { .x = @as(f32, @floatFromInt(40*i)), .y = 0 };
-                    const endVector = rl.Vector2 { .x = @as(f32, @floatFromInt(40*i)), .y = kScreenHeight };
+    //             for (0..kScreenWidth/40) |i| {
+    //                 const startVector = rl.Vector2 { .x = @as(f32, @floatFromInt(40*i)), .y = 0 };
+    //                 const endVector = rl.Vector2 { .x = @as(f32, @floatFromInt(40*i)), .y = kScreenHeight };
 
-                    rl.drawLineV(startVector, endVector, rl.Color.light_gray);
-                }
+    //                 rl.drawLineV(startVector, endVector, rl.Color.light_gray);
+    //             }
 
-                for (0..kScreenHeight/40) |i| {
-                    const startVector = rl.Vector2 { .y = @as(f32, @floatFromInt(40*i)), .x = 0 };
-                    const endVector = rl.Vector2 { .y = @as(f32, @floatFromInt(40*i)), .x = kScreenWidth };
+    //             for (0..kScreenHeight/40) |i| {
+    //                 const startVector = rl.Vector2 { .y = @as(f32, @floatFromInt(40*i)), .x = 0 };
+    //                 const endVector = rl.Vector2 { .y = @as(f32, @floatFromInt(40*i)), .x = kScreenWidth };
 
-                    rl.drawLineV(startVector, endVector, rl.Color.light_gray);
-                }
+    //                 rl.drawLineV(startVector, endVector, rl.Color.light_gray);
+    //             }
 
-                rl.drawText("Hello World", 190, 200, 20, rl.Color.light_gray);
-            }
-        }
+    //             rl.drawText("Hello World", 190, 200, 20, rl.Color.light_gray);
+    //         }
+    //     }
 
-        rl.beginDrawing();
-        defer rl.endDrawing();
-        rl.clearBackground(rl.Color.black);
-        rl.drawTextureRec(screenCamera1.texture, sourceRectangle, rl.Vector2{.x = 0, .y = 0}, rl.Color.white);
-    }
+    //     rl.beginDrawing();
+    //     defer rl.endDrawing();
+    //     rl.clearBackground(rl.Color.black);
+    //     rl.drawTextureRec(screenCamera1.texture, sourceRectangle, rl.Vector2{.x = 0, .y = 0}, rl.Color.white);
+    // }
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // // stdout is for the actual output of your application, for example if you
+    // // are implementing gzip, then only the compressed bytes should be sent to
+    // // stdout, not any debugging messages.
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
-    try bw.flush(); // don't forget to flush!
+    // try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {
